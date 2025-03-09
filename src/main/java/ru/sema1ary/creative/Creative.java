659f0f83b21d2178b1f6f8cf9d5bbc696e8ecaee
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-09 23:27:52 +0300

    Изменил немного структуру

--- src/main/java/ru/sema1ary/creative/Creative.java
+++ src/main/java/ru/sema1ary/creative/Creative.java
@@ -3,15 +3,13 @@ package ru.sema1ary.creative;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.sema1ary.creative.listener.EntitySpawnListener;
 import ru.sema1ary.creative.listener.PortalCreationListener;
-import ru.sema1ary.creative.listener.SpectateListener;
-import ru.sema1ary.creative.listener.TeleportListener;
+import ru.sema1ary.creative.listener.SpectatatorListener;
 
 public final class Creative extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new TeleportListener(), this);
-        getServer().getPluginManager().registerEvents(new SpectateListener(), this);
+        getServer().getPluginManager().registerEvents(new SpectatatorListener(), this);
         getServer().getPluginManager().registerEvents(new EntitySpawnListener(), this);
         getServer().getPluginManager().registerEvents(new PortalCreationListener(), this);
     }
--- src/main/java/ru/sema1ary/creative/listener/SpectatatorListener.java
+++ src/main/java/ru/sema1ary/creative/listener/SpectatatorListener.java
@@ -3,11 +3,20 @@ package ru.sema1ary.creative.listener;
 import com.destroystokyo.paper.event.player.PlayerStartSpectatingEntityEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
-public class SpectateListener implements Listener {
+public class SpectatatorListener implements Listener {
     @EventHandler
     public void onSpectate(PlayerStartSpectatingEntityEvent event) {
         if(!event.getPlayer().hasPermission("creative.spectate")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent event) {
+        if(event.getCause().equals(PlayerTeleportEvent.TeleportCause.SPECTATE) &&
+                !event.getPlayer().hasPermission("creative.spectator.teleport")) {
             event.setCancelled(true);
         }
     }
