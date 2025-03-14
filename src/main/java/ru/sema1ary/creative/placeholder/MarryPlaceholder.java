727033ac836a3a58a2b0c132faf697c1c83db32d
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-14 23:51:16 +0300

    Плейсхолдер женитьб

--- src/main/java/ru/sema1ary/creative/placeholder/MarryPlaceholder.java
+++ src/main/java/ru/sema1ary/creative/placeholder/MarryPlaceholder.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 public class MarryPlaceholder extends PlaceholderExpansion {
     @Override
     public @NotNull String getIdentifier() {
-        return "creative";
+        return "marry";
     }
 
     @Override
